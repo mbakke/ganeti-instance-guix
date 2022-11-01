bc8ba30823547569e2bb3503d6bbb32215e4b834
Marius Bakke <marius@gnu.org>
2022-11-01 23:34:59 +0100

    Apply guix style on examples.
    * examples/config-base.scm: Apply 'guix style' (with minor tweaks).
    * examples/dynamic-btrfs.scm.in: Likewise.
    * examples/dynamic-lvm.scm.in: Likewise.

--- examples/config-base.scm
+++ examples/config-base.scm
@@ -1,8 +1,8 @@
 (use-modules (gnu)
              (ice-9 match)
              (srfi srfi-60)
-	     (ice-9 textual-ports)
-	     (guix build utils)
+             (ice-9 textual-ports)
+             (guix build utils)
              (rnrs io ports))
 (use-package-modules certs screen linux)
 (use-service-modules networking ssh sysctl)
@@ -10,84 +10,94 @@
 (define (cidr->netmask address)
   "Convert a CIDR specification such as 10.0.0.0/24 to 255.255.255.0."
   (let ((mask (string->number (match (string-split address #\/)
-                                     ((address mask) mask)
-                                     (_ "32")))))
+                                ((address mask) mask)
+                                (_ "32")))))
     (inet-ntop AF_INET
                (arithmetic-shift (inet-pton AF_INET "255.255.255.255")
                                  (- 32 mask)))))
 
 (define default-os
   (operating-system
-   (host-name (getenv "INSTANCE_NAME"))
-   ;; Take the hosts time zone, sans trailing newline if present.
-   (timezone (string-trim-right
-              (call-with-input-file "/etc/timezone"
-		(lambda (port)
-                  (get-string-all port)))
-              #\newline))
-   (locale "en_US.utf8")
-   
-   (kernel-arguments '("console=ttyS0"))
+    (host-name (getenv "INSTANCE_NAME"))
+    ;; Take the hosts time zone, sans trailing newline if present.
+    (timezone (string-trim-right (call-with-input-file "/etc/timezone"
+                                   (lambda (port)
+                                     (get-string-all port)))
+                                 #\newline))
+    (locale "en_US.utf8")
 
-   ;; Boot in "legacy" BIOS mode, assuming /dev/sdX is the
-   ;; target hard disk
-   ;; (bootloader (bootloader-configuration
-   ;;              (bootloader grub-bootloader)
-   ;;              (targets '("/dev/vda"))))
-   (bootloader (bootloader-configuration
-                (bootloader grub-bootloader)
-                (targets `(,(getenv "TARGET_DEVICE")))))
-   (file-systems
-    (cons*
-     (file-system
-      (device (file-system-label (string-append (getenv "INSTANCE_NAME") "-system")))
-      (mount-point "/")
-      (type (or (getenv "OSP_FILESYSTEM")
-                "ext4")))
-     %base-file-systems))
-   ;; This is where user accounts are specified.  The "root"
-   ;; account is implicit, and is initially created with the
-   ;; empty password.
-   (users %base-user-accounts)
+    (kernel-arguments '("console=ttyS0"))
 
-   ;; Globally-installed packages.
-   (packages (cons* lvm2 screen nss-certs btrfs-progs xfsprogs %base-packages))
+    ;; Boot in "legacy" BIOS mode, assuming /dev/sdX is the
+    ;; target hard disk
+    ;; (bootloader (bootloader-configuration
+    ;; (bootloader grub-bootloader)
+    ;; (targets '("/dev/vda"))))
+    (bootloader (bootloader-configuration
+                  (bootloader grub-bootloader)
+                  (targets `(,(getenv "TARGET_DEVICE")))))
+    (file-systems (cons* (file-system
+                           (device (file-system-label
+                                    (string-append (getenv "INSTANCE_NAME")
+                                                   "-system")))
+                           (mount-point "/")
+                           (type (or (getenv "OSP_FILESYSTEM") "ext4")))
+                         %base-file-systems))
+    ;; This is where user accounts are specified.  The "root"
+    ;; account is implicit, and is initially created with the
+    ;; empty password.
+    (users %base-user-accounts)
 
-   ;; Add services to the baseline
-   (services
-    (let ((myip (getenv "NIC_0_IP"))
-          (mysubnet (getenv "NIC_0_NETWORK_SUBNET"))
-          (mygateway (getenv "NIC_0_NETWORK_GATEWAY"))
-          (root-authorizations
-           (cond ((file-exists? "/etc/ssh/authorized_keys.d/root")
-                  "/etc/ssh/authorized_keys.d/root")
-                 ((file-exists? "/root/.ssh/authorized_keys")
-                  "/root/.ssh/authorized_keys")
-                 (else #f))))
-      (cons* (service static-networking-service-type
-		      (list (static-networking
-			     (addresses
-			      (list (network-address
-			             (device "eth0")
-			             (value (string-append myip "/" (cadr (string-split mysubnet #\/)))))))
-			     (routes
-			      (list (network-route
-				     (destination "default")
-				     (gateway mygateway))))
-			     (name-servers (list mygateway)))))
+    ;; Globally-installed packages.
+    (packages (cons* lvm2
+                     screen
+                     nss-certs
+                     btrfs-progs
+                     xfsprogs
+                     %base-packages))
 
-	     (service openssh-service-type
-                      (openssh-configuration
-                       (permit-root-login 'prohibit-password)
-		       (x11-forwarding? #t)
-                       (authorized-keys
-                        (if root-authorizations
-                            `(("root" ,(local-file root-authorizations)))
-                            '()))))
-             (modify-services %base-services
-			      (sysctl-service-type config =>
-                                                   (sysctl-configuration
-                                                    (settings (append '(("vm.swappiness" . "10"))
-								      %default-sysctl-settings))))))))
-   ;; Allow resolution of '.local' host names with mDNS.
-   (name-service-switch %mdns-host-lookup-nss)))
+    ;; Add services to the baseline
+    (services
+     (let ((myip (getenv "NIC_0_IP"))
+           (mysubnet (getenv "NIC_0_NETWORK_SUBNET"))
+           (mygateway (getenv "NIC_0_NETWORK_GATEWAY"))
+           (root-authorizations (cond
+                                  ((file-exists?
+                                    "/etc/ssh/authorized_keys.d/root")
+                                   "/etc/ssh/authorized_keys.d/root")
+                                  ((file-exists? "/root/.ssh/authorized_keys")
+                                   "/root/.ssh/authorized_keys")
+                                  (else #f))))
+       (append
+        (list (service static-networking-service-type
+                       (list (static-networking
+                              (addresses
+                               (list (network-address
+                                      (device "eth0")
+                                      (value (string-append
+                                              myip "/"
+                                              (cadr (string-split mysubnet
+                                                                  #\/)))))))
+                              (routes
+                               (list (network-route
+                                      (destination "default")
+                                      (gateway mygateway))))
+                              (name-servers (list mygateway)))))
+
+              (service openssh-service-type
+                       (openssh-configuration
+                        (permit-root-login 'prohibit-password)
+                        (x11-forwarding? #t)
+                        (authorized-keys (if root-authorizations
+                                             `(("root" ,(local-file
+                                                         root-authorizations)))
+                                             '())))))
+        (modify-services %base-services
+          (sysctl-service-type
+           config =>
+           (sysctl-configuration
+            (settings (append '(("vm.swappiness" . "10"))
+                              %default-sysctl-settings))))))))
+
+    ;; Allow resolution of '.local' host names with mDNS.
+    (name-service-switch %mdns-host-lookup-nss)))
