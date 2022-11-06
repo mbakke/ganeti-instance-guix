e83cb35fc584b6d7c9442723a487e7255de6f7d1
Marius Bakke <marius@gnu.org>
2022-11-06 17:41:44 +0100

    Add tests for file system creation.
    * tests/file-system.sh: New file.
    * Makefile.am (TESTS): Add it.

---
+++ tests/file-system.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+. ./common.sh
+
+DISK_IMAGE="$(mktemp)"
+CLEANUP+="rm $DISK_IMAGE"
+
+fallocate -l 1GiB "$DISK_IMAGE"
+
+# ext support is mandatory.
+format_device "$DISK_IMAGE" ext4
+format_device "$DISK_IMAGE" ext4 test-label
+tune2fs -l "$DISK_IMAGE" | grep test-label
+
+if [ -x "$MKFS_BTRFS" ]; then
+    format_device "$DISK_IMAGE" btrfs
+fi
+
+if [ -x "$MKFS_XFS" ]; then
+    format_device "$DISK_IMAGE" xfs
+fi
+
+if [ -x "$MKFS_F2FS" ]; then
+    format_device "$DISK_IMAGE" f2fs
+fi
+
+! format_device "$DISK_IMAGE" unknown-fs
